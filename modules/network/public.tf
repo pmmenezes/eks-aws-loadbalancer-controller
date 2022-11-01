ede412ce71d5e1898e4120ca675c830b1fb07a65
Pablo M Menezes <pmenezes.aju@gmail.com>
2022-11-01 11:51:50 -0300

    Add rota na subnet-public-1c

--- modules/network/public.tf
+++ modules/network/public.tf
@@ -30,3 +30,9 @@ resource "aws_route_table_association" "public_1a" {
   route_table_id = aws_route_table.igw_route_table.id
 
 }
+
+resource "aws_route_table_association" "public_1c" {
+  subnet_id      = aws_subnet.public_subnet_1c.id
+  route_table_id = aws_route_table.igw_route_table.id
+
+}
